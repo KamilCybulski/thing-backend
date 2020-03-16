f38b0c8931be4ed441eef04071caaa94da243646
zuy_szatan <kamillcybulski@gmail.com>
2020-03-16 23:22:35 +0100

    Add db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppGateway } from './app.gateway';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [`${__dirname}/../**/*.entity.{ts,js}`],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, AppGateway],
 })
