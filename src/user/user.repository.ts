5692f57937eaeb54fc664b15d482fe861e051365
zuy_szatan <kamillcybulski@gmail.com>
2020-03-22 12:40:10 +0100

    Prevent users with the same name

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,3 +1,4 @@
+import { ConflictException } from '@nestjs/common';
 import { CredentialsDTO } from 'src/auth/dtos';
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -5,9 +6,16 @@ import { User } from './user.entity';
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(dto: CredentialsDTO): Promise<User> {
+    const { username, password } = dto;
+
+    const count = await this.count({ name: username });
+    if (count > 0) {
+      throw new ConflictException();
+    }
+
     const user = new User();
-    user.name = dto.username;
-    user.password = dto.password;
+    user.name = username;
+    user.password = password;
     await user.save();
     return user;
   }
