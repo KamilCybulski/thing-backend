5f636c37dfd2eb06db8415f1d9461f3e40e463e5
Kamil <kamillcybulski@gmail.com>
2020-03-28 08:39:56 +0100

    Add documentation (#10)

--- src/auth/dtos/credentials.dto.ts
+++ src/auth/dtos/credentials.dto.ts
@@ -1,9 +1,18 @@
 import { Length } from 'class-validator';
+import {  ApiProperty} from '@nestjs/swagger';
 
 export class CredentialsDTO {
+  @ApiProperty({
+    minLength: 3,
+    maxLength: 20,
+  })
   @Length(3, 20)
   username: string;
 
+  @ApiProperty({
+    minLength: 8,
+    maxLength: 20,
+  })
   @Length(8, 20)
   password: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ credentials: true, origin: process.env.FRONTEND_BASE_URL });
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('Thing Backend')
+    .setDescription('Api documentation')
+    .setVersion('1.0')
+    .addTag('Auth')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
