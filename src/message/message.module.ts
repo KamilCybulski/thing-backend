626a511895a7f1127ae86c3d5566288230025e17
zuy_szatan <kamillcybulski@gmail.com>
2020-04-05 21:16:32 +0200

    add endpoint

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -2,10 +2,12 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { MessageService } from "./message.service";
 import { MessageRepository } from "./message.repository";
+import { MessageController } from './message.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([MessageRepository])],
   providers: [MessageService],
   exports: [MessageService],
+  controllers: [MessageController],
 })
 export class MessageModule {}
